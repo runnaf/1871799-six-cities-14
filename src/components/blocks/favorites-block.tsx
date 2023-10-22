ebdd3da9b3447d68a28edc2a82d5ad4530173a0a
“Irina <runnaf@mail.ru>
2023-10-22 23:44:11 +0300

    added return for block favorites

--- src/components/blocks/favorites-block.tsx
+++ src/components/blocks/favorites-block.tsx
@@ -6,30 +6,36 @@ type TCityArray = string[];
 const CITYARRAY: TCityArray = ['Amsterdam', 'Paris', 'Cologne', 'Brussels', 'Hamburg', 'Dusseldorf'];
 
 function getCardFavoritesCity(arrayCity: TCityArray, data: TCardProps): JSX.Element {
-  arrayCity.forEach((element)=>{
-    const filterArray = data.filter((itemFilter)=> itemFilter.city.name === element && itemFilter.isFavorite);
-    if (filterArray.length > 0) {
-      return (
-        <li className="favorites__locations-items">
-          <div className="favorites__locations locations locations--current">
-            <div className="locations__item">
-              <a className="locations__item-link" href="#">
-                <span>{element}</span>
-              </a>
-            </div>
-          </div>
-          <div className="favorites__places">
-            {filterArray.map((item): JSX.Element => {
-              const {previewImage, isPremium, price, rating, title, type, isFavorite, id} = item;
-              return (
-                <Card previewImage={previewImage} isPremium={isPremium} price={price} rating={rating} title={title} type={type} isFavorite={isFavorite} card={'favorites__card'} wrapper={'favorites__image-wrapper'} cardInfo={'favorites__card-info'} key={id} classNamePremium = {'place-card__mark'}/>
-              );
-            })}
-          </div>
-        </li>
-      );
-    }
-  });
+  return (
+    <>
+      {
+        arrayCity.forEach((element)=>{
+          const filterArray = data.filter((itemFilter)=> itemFilter.city.name === element && itemFilter.isFavorite);
+          if (filterArray.length > 0) {
+            return (
+              <li className="favorites__locations-items">
+                <div className="favorites__locations locations locations--current">
+                  <div className="locations__item">
+                    <a className="locations__item-link" href="#">
+                      <span>{element}</span>
+                    </a>
+                  </div>
+                </div>
+                <div className="favorites__places">
+                  {filterArray.map((item): JSX.Element => {
+                    const {previewImage, isPremium, price, rating, title, type, isFavorite, id} = item;
+                    return (
+                      <Card previewImage={previewImage} isPremium={isPremium} price={price} rating={rating} title={title} type={type} isFavorite={isFavorite} card={'favorites__card'} wrapper={'favorites__image-wrapper'} cardInfo={'favorites__card-info'} key={id} classNamePremium = {'place-card__mark'}/>
+                    );
+                  })}
+                </div>
+              </li>
+            );
+          }
+        })
+      }
+    </>
+  );
 }
 
 export function FavoritesBlock(): JSX.Element {
