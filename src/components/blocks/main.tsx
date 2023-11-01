755ae09ca17caa33c05af97bd46c483f0c99dd3d
“Irina <runnaf@mail.ru>
2023-11-01 18:01:57 +0300

    fixed conflict

--- src/components/blocks/main.tsx
+++ src/components/blocks/main.tsx
@@ -1,12 +1,11 @@
 import { ListLocation, ListPlacesOptions } from '../ui/list-main';
 import { DataMain } from './data/data-main';
 import { DataMainClassName } from './data/data-class-names';
-import { TCardProps } from './data/data-cities-card';
+import { DataCities } from './data/data-cities-card';
 import { Card, TCard } from './card';
 import { addPluralEnging } from '../../utils/common';
-import { CityMap } from '../../const';
 import { useState } from 'react';
-import { MapAdded } from './map';
+import { CityMap } from '../../const';
 
 export type TMainBlocks= {
     placesOptions: TMainItem[];
@@ -31,11 +30,14 @@ export type TClassName = {
     isActive: string;
 }
 
-export function Main({offers} : {offers: TCardProps}): JSX.Element {
-  const activeCity = CityMap.Amsterdam;
-  const count = offers.filter((item) => item.city.name === CityMap.Amsterdam.name).length;
+const CITY = 'Amsterdam';
+const activeCity = CityMap.Amsterdam;
+const count = DataCities.filter((item) => item.city.name === CITY).length;
+
+export function Main(): JSX.Element {
   const [hoveredOfferId, setHoveredOfferId] = useState<
-    TCard['id'] | null > (null);
+  TCard['id'] | null>(null);
+  console.log(hoveredOfferId);
 
   function handleCardHover(offerId: TCard['id'] | null) {
     setHoveredOfferId(offerId);
@@ -53,7 +55,7 @@ export function Main({offers} : {offers: TCardProps}): JSX.Element {
         <div className="cities__places-container container">
           <section className="cities__places places">
             <h2 className="visually-hidden">Places</h2>
-            <b className="places__found">{count} place{addPluralEnging(count)} to stay in {activeCity.name}</b>
+            <b className="places__found">{count} place{addPluralEnging(count)} to stay in {activeCity}</b>
             <form className="places__sorting" action="#" method="get">
               <span className="places__sorting-caption">Sort by</span>
               <span className="places__sorting-type" tabIndex={0}>
@@ -65,8 +67,8 @@ export function Main({offers} : {offers: TCardProps}): JSX.Element {
               <ListPlacesOptions classNames={DataMainClassName.placesClassListNames} itemsList={DataMain.placesOptions} classNameItems={DataMainClassName.placesClassItemNames} />
             </form>
             <div className="cities__places-list places__list tabs__content">
-              {offers.map((item) => (
-                item.city.name === activeCity.name && <Card block="cities" offer={item} key={item.id} onCardHover={handleCardHover}/>
+              {DataCities.map((item) => (
+                item.city.name === CITY && <Card block="cities" offer={item} key={item.id} onCardHover={handleCardHover}/>
               ))}
             </div>
           </section>
