da1df8099067e873f97abc043f3d5a9c85b1fbdf
“Irina <runnaf@mail.ru>
2023-11-02 17:59:35 +0300

    fixed rating in form component

--- src/components/blocks/review-form/review-form.tsx
+++ src/components/blocks/review-form/review-form.tsx
@@ -26,10 +26,9 @@ export function ReviewForm() {
   }
 
   function submitHandler (evt: ChangeEvent<HTMLFormElement>) {
-    console.log(comment, rating)
     evt.preventDefault();
     setRating('');
-    setComment('')
+    setComment('');
   }
 
   return (
@@ -41,7 +40,7 @@ export function ReviewForm() {
           .map(([score, title])=>(
             <Fragment key={score}>
               <input className="form__rating-input visually-hidden" name="rating" value={score} id={`${score}-stars`} type="radio" checked={rating === score} onChange={handleInputChange} />
-              <label htmlFor="5-stars" className="reviews__rating-label form__rating-label" title={title}>
+              <label htmlFor={`${score}-stars`} className="reviews__rating-label form__rating-label" title={title}>
                 <svg className="form__star-image" width={37} height={33}>
                   <use xlinkHref="#icon-star"></use>
                 </svg>
