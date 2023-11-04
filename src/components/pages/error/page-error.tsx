5be704092e6d76df4a0f07e68d511c883c1525cc
“Irina <runnaf@mail.ru>
2023-11-04 12:59:19 +0300

    added style for error page

--- src/components/pages/error/page-error.tsx
+++ src/components/pages/error/page-error.tsx
@@ -7,17 +7,20 @@ import style from './page-error.module.css';
 
 export function Error(): JSX.Element {
   return (
-    <section className={style.wrapper}>
-      <Helmet>
-        <title>6 Cities: Page not found</title>
-      </Helmet>
-      <Logo classNameLinks={DataHeader.classNameLinks} classNameImages={DataHeader.classNameImages} width={DataHeader.width} height={DataHeader.height}/>
-      <div className={style.container}>
-        <p className={style.title}>Page Not Found :(</p>
-        <Link className={style.link} to={AppRoute.Root}>
-          <span>Back</span>
-        </Link>
-      </div>
+    <section className={`${style.wrapper} page page--gray container` }>
+        <Helmet>
+          <title>6 Cities: Page not found</title>
+        </Helmet>
+        <div className='header__wrapper'>
+          <Logo classNameLinks={DataHeader.classNameLinks} classNameImages={DataHeader.classNameImages} width={DataHeader.width} height={DataHeader.height}/>
+        </div>        
+        <div className={style.container}>
+          <h2 className={style.title}>Page Not Found :(</h2>
+          <p className={style.description}>We're sorry. the page requested could not be found</p>
+          <Link className={style.link} to={AppRoute.Root}>
+            <span>Go to homepage</span>
+          </Link>
+        </div>
     </section>
   );
 }
